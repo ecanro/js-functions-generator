6f0a5ed3a65cab7d6ff7b4f3c563b37aa36d32bf
edgar canro <edgar.canro@gmail.com>
2020-02-03 21:10:24 +0000

    Map e no map, para objetos

---
+++ Map.js
@@ -0,0 +1,25 @@
+//Map y no map
+//https://wiki.developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Map
+
+var miMapa = new Map();
+
+var claveObj = {},
+    claveFunc = function () {},
+    claveCadena = "una cadena";
+
+// asignando valores
+miMapa.set(claveCadena, "valor asociado con 'una cadena'");
+miMapa.set(claveObj, "valor asociado con claveObj");
+miMapa.set(claveFunc, "valor asociado with claveFunc");
+
+miMapa.size; // 3
+
+// obteniendo los valores
+miMapa.get(claveCadena);    // "valor asociado con 'una cadena'"
+miMapa.get(claveObj);       // "valor asociado con claveObj"
+miMapa.get(claveFunc);      // "valor asociado con claveFunc"
+
+miMapa.get("una cadena");   // ""valor asociado con 'una cadena'"
+                         // porque claveCadena === 'una cadena'
+miMapa.get({});           // undefined, porque claveObj !== {}
+miMapa.get(function() {}) // undefined, porque claveFunc !== function () {}
